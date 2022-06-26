af3174b96682f96c0c153249c4d1cdd5d360e8ad
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-06-27 01:04:32 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import NavigationIcon from '@mui/icons-material/Navigation';
-import './App.css';
 
 export default function FloatingActionButtons() {
   return (
